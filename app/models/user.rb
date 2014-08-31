ed01b8665d4d05b245a97b96040daf04fa994bf2
viniciusdovale <viniciusdovale10@gmail.com>
2014-08-31 00:27:08 -0300

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,4 @@ class User < ActiveRecord::Base
 	attr_accessible :bio, :email, :full_name, :location, :password
 
   	validates_presence_of :email, :full_name, :location, :password
-  	validates_confirmation_of :password	
 end
